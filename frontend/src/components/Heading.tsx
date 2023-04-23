128567cd5d1546fd7a12ecd7725fbc8e77815d25
Tommaso Amici <me@tommasoamici.com>
2023-04-23 15:06:58 +0200

    fix: improved styling of Heading component

--- frontend/src/components/Heading.tsx
+++ frontend/src/components/Heading.tsx
@@ -1,6 +1,9 @@
 import { cx } from "classix";
 
-type HLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+export const sizes = ["h1", "h2", "h3", "h4", "h5", "h6"] as const;
+
+type HLevel = (typeof sizes)[number];
+
 type Props = {
   as: HLevel;
   size?: HLevel;
@@ -9,15 +12,15 @@ type Props = {
 };
 
 const styles: Record<HLevel, string> = {
-  h1: "text-4xl mb-2",
-  h2: "text-3xl mb-2",
-  h3: "text-2xl mb-1",
-  h4: "text-xl mb-1",
-  h5: "text-lg mb-0.5",
-  h6: "text-base mb-0.5",
+  h1: "text-5xl font-bold mb-2",
+  h2: "text-4xl font-bold mb-2",
+  h3: "text-3xl font-bold mb-1",
+  h4: "text-2xl font-semibold mb-1",
+  h5: "text-xl font-semibold mb-0.5",
+  h6: "text-lg font-semibold mb-0.5",
 };
 
 export function Heading({ as, size, children, className }: Props) {
   const Tag = as;
-  return <Tag className={cx("font-semibold", styles[size ?? as], className)}>{children}</Tag>;
+  return <Tag className={cx(styles[size ?? as], className)}>{children}</Tag>;
 }
