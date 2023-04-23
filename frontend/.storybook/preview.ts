646af3bf58be694d97a4f6bde13f0d9637bbfc67
Tommaso Amici <me@tommasoamici.com>
2023-04-23 15:04:44 +0200

    feat: added storybook

---
+++ frontend/.storybook/preview.ts
@@ -0,0 +1,18 @@
+import type { Preview } from "@storybook/react";
+
+import "../src/app/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    layout: "centered",
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
